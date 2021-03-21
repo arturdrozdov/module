c3278d57c6d69ffa0cdadbb6cc4154c18891dac7
Drozdov Artur <artur.drozdov11@gmail.com>
2021-03-21 10:57:56 +0200

    working

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.sliderGallery').slick({
+      autoplay:true,
+      arrows: true,
+      prevArrow:"<button type='button' class='slick-prev pull-left'><i class='icon-angle-left' aria-hidden='true'></i></button>",
+      nextArrow:"<button type='button' class='slick-next pull-right'><i class='icon-angle-right' aria-hidden='true'></i></button>"
+  });
+});
